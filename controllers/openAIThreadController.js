dd7f45fb8c31f6c4ec40c75c7ea770f1d43efb52
abhay <abhaym1205@gmail.com>
2024-03-12 15:04:36 +0530

    solved  mongo db api create bug

--- controllers/openAIThreadController.js
+++ controllers/openAIThreadController.js
@@ -40,17 +40,18 @@ const callAssistant = async (data) => {
   // run thread
   const run = await openai.beta.threads.runs.create(thread.id, {
     assistant_id: assistantID,
-    instructions: `${data} Here are the abbreviations akiec = Actinic keratoses and intraepithelial carcinoma / Bowen's disease, bcc = Basal cell carcinoma, bkl = Benign keratosis-like lesions (solar lentigines / seborrheic keratoses and lichen-planus like keratoses) df = Dermatofibroma, mel = Melanoma, nv = Melanocytic nevi, vasc = Vascular lesions (angiomas, angiokeratomas, pyogenic granulomas and hemorrhage)._inference = The disease that has highest possible chance of happening on the basis of percentages of different diseases. In above format the numbers folowing the labels denote the % likliness of the affected by corresponding disease while the _inference tag denote the disease that has highest possible chance of happening. Describe what is the disease basal cell carcinoma if it's the disease. Don't need to end the ans in open ended manner so no would you like to know more? style of questions. Give your analyisis based on what the patient is suffering from the data is provided to you in the format in this prompt itself at the start of the text.
+    instructions: `Here are the abbreviations akiec = Actinic keratoses and intraepithelial carcinoma / Bowen's disease, bcc = Basal cell carcinoma, bkl = Benign keratosis-like lesions (solar lentigines / seborrheic keratoses and lichen-planus like keratoses) df = Dermatofibroma, mel = Melanoma, nv = Melanocytic nevi, vasc = Vascular lesions (angiomas, angiokeratomas, pyogenic granulomas and hemorrhage)._inference = The disease that has highest possible chance of happening on the basis of percentages of different diseases. In above format the numbers folowing the labels denote the % likliness of the affected by corresponding disease while the _inference tag denote the disease that has highest possible chance of happening. Describe what is the disease basal cell carcinoma if it's the disease. Don't need to end the ans in open ended manner so no would you like to know more? style of questions. Give your analyisis based on what the patient is suffering from the data is provided to you in the format in this prompt itself at the start of the text.
       {
       akiec: "0.0"
-      bcc: "99.97"
+      bcc: ""
       bkl: "0.0"
       df: "0.0"
       mel: "0.0"
-      nv: "0.030000001"
+      nv: "0.030000000"
       vasc: "0.0"
-      _inference: "bcc"
-  }`,
+      _inference: "dummy data"
+  } (Ignore this data this is the format)
+  ${data}(This is the data you should focus on here)`,
   });
   // get thread op
   await waitForCompletion(run.status, run.id, thread.id);
@@ -65,19 +66,15 @@ export const generateQuiz = async (req, res) => {
   console.log(patientData);
   try {
     const data = await callAssistant(patientData);
-    Reports.create({ Parameters: patientData, analysis: data });
+    const result = await Reports.create({ Parameters: patientData, analysis: data });
 
-    const report = await Reports.findOne({
-      Parameters: patientData,
-      analysis: data,
+    if (result)
+      console.log("this is report data response", result)
+    res.status(200).send({
+      success: true,
+      id: result._id,
+      data: result.analysis,
     });
-
-    if (data)
-      res.status(200).send({
-        success: true,
-        id: report.id,
-        data,
-      });
   } catch (e) {
     console.log(e);
     res.status(500).json({
